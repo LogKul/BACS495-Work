3e493dd37e42e28bb70c29a9c1f4e6fafe7eccbd
prilika1 <53357004+prilika1@users.noreply.github.com>
2022-02-04 20:16:06 -0700

    Created To-Do Web-App

---
+++ todolist/src/AddLine.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function AddLine() {
+    const [field_text, setText] = useState("");
+    const [todo_items, setItems] = useState([])
+
+    const handleSubmit = (event) => {
+        /*alert(field_text);*/
+        if (field_text) {
+            setItems(todo_items.concat(field_text))
+        }
+        setText("");
+        event.preventDefault();
+    }
+
+    const handleRemove = (event) => {
+        const updatedItems = todo_items.filter((item) => item !== event.target.id);
+
+        setItems(updatedItems)
+    }
+
+    return (
+        <div className="App-content">
+            <form onSubmit={handleSubmit} className="Item-form">
+                <label>To-Do Item: </label>
+                <input
+                    type="text"
+                    value={field_text}
+                    onChange={(e) => setText(e.target.value)}
+                />
+                <button type="submit">
+                    Add
+                </button>
+            </form>
+            <ul>
+                {todo_items.map(item => (
+                    <li key={item}>
+                        <button type="button" id={item} onClick={handleRemove}>
+                            Done
+                        </button>
+                        To-Do: <span className="Activity">{item}</span>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default AddLine;
---
+++ todolist/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import AddLine from './AddLine.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>To-Do Website</h1>
+      </header>
+      <AddLine />
+    </div>
+  );
+}
+
+export default App;
