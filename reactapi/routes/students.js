e6fec46b232f17dc4d0c76c0d9c677315a5a6ea4
prilika1 <53357004+prilika1@users.noreply.github.com>
2022-02-21 18:16:41 -0700

    Updated all CRUD, original is commented

--- reactapi/routes/students.js
+++ reactapi/routes/students.js
@@ -1,26 +1,20 @@
 var express = require('express');
-const { report } = require('../app');
+const { ObjectId, ObjectID } = require('mongodb');
+//const { report } = require('../app');*/
 var router = express.Router();
 
-var students = [
-  { id: 1, firstName: "Jeff" },
-  { id: 2, firstName: "Jesse" },
-  { id: 3, firstName: "Harolde" },
-  { id: 4, firstName: "Derek" },
-  { id: 5, firstName: "Funny" }
-]
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.send('Base student path. Nothing here.');
 });
 
-router.get('/students', function (req, res, next) {
-  res.json(students);
-});
+router.get('/', function (req, res, next) {
+  var db = req.app.locals.db;
+  const query = { _id: ObjectID(req.body.id) };
+  db.collection("users").findOne(query).then(function (result) { res.json(result) });
 
-router.get('/:id', function (req, res, next) {
-  var flag = false;
+  /*var flag = false;
   for (let student of students) {
     if (student.id === parseInt(req.params.id)) {
       var temp = student;
@@ -32,11 +26,22 @@ router.get('/:id', function (req, res, next) {
   }
   else {
     res.send('ERROR: ID NOT FOUND FOR ID = ' + req.params.id);
-  }
+  }*/
 });
 
-router.put('/:id/:firstName', function (req, res, next) {
-  var flag = false;
+router.put('/', function (req, res, next) {
+  const filter = { _id: ObjectID(req.body.id) };
+  const update = {
+    $set: {
+      "name": req.body.name,
+      "type": req.body.type
+    }
+  }
+  var db = req.app.locals.db;
+  db.collection("users").updateOne(filter, update);
+  res.send("Updated etc");
+
+  /*var flag = false;
   for (var i = 0; i < students.length; i++) {
     if (students[i].id === parseInt(req.params.id)) {
       flag = true;
@@ -50,11 +55,19 @@ router.put('/:id/:firstName', function (req, res, next) {
   else {
     students.push({ id: parseInt(req.params.id), firstName: req.params.firstName })
     res.send('Student added successfully!');
-  }
+  }*/
 });
 
-router.post('/:id/:firstName', function (req, res, next) {
-  var flag = false;
+router.post('/', function (req, res, next) {
+  const user = {
+    "name": req.body.name,
+    "type": req.body.type
+  }
+  var db = req.app.locals.db;
+  db.collection("users").insertOne(user);
+  res.send("User inserted");
+
+  /*var flag = false;
   for (var i = 0; i < students.length; i++) {
     if (students[i].id === parseInt(req.params.id)) {
       flag = true;
@@ -66,11 +79,15 @@ router.post('/:id/:firstName', function (req, res, next) {
   else {
     students.push({ id: parseInt(req.params.id), firstName: req.params.firstName })
     res.send('Student posted to database successfully!');
-  }
+  }*/
 });
 
-router.delete('/:id', function (req, res, next) {
-  var flag = false;
+router.delete('/', function (req, res, next) {
+  var db = req.app.locals.db;
+  const query = { _id: ObjectID(req.body.id) };
+  db.collection("users").deleteOne(query).then(function (result) { res.json(result) });
+
+  /*var flag = false;
   for (var i = 0; i < students.length; i++) {
     if (students[i].id === parseInt(req.params.id)) {
       students.splice(i, 1);
@@ -83,18 +100,7 @@ router.delete('/:id', function (req, res, next) {
   }
   else {
     res.send('ERROR: ID NOT FOUND FOR ID = ' + req.params.id);
-  }
-});
-
-/*test*/
-router.post('/', function (req, res, next) {
-  const user = {
-    "name": req.body.name,
-    "type": req.body.type
-  }
-  var db = req.app.locals.db
-  db.collection("users").insertOne(user);
-  res.send("User inserted")
+  }*/
 });
 
 module.exports = router;
